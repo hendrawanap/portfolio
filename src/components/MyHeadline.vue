26bd501f261caee96a6a920867bdfef15d909b68
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-03-24 17:07:35 +0700

    Apply responsive layout for headline section

--- src/components/MyHeadline.vue
+++ src/components/MyHeadline.vue
@@ -2,22 +2,23 @@
 </script>
 
 <template>
-  <section>
-    <p class="mono text-primary text-sm mb-4">Hi, my name is</p>
-    <h2 class="text-opacity-80 mb-4">
-      Hendrawan.
+  <section class="mx-auto max-w-5xl flex flex-col justify-center items-start">
+    <p class="mono text-primary text-sm mb-4 md:text-base">Hi, my name is</p>
+    <h2 class="text-opacity-80 mb-4 md:text-6xl xl:text-7xl">
+      Hendrawan<span class="hidden text-opacity-80 lg:inline"> Anantha</span>.
     </h2>
-    <h2 class="text-opacity-50 mb-6">
+    <h2 class="text-opacity-50 mb-6 md:text-6xl xl:text-7xl">
       I build website for your stuff.
     </h2>
-    <p class="text-opacity-50 mb-12">
+    <p class="text-opacity-50 mb-12 md:text-lg max-w-xl">
       I’m an informatics engineering students and a fullstack web developer.
       Currently, I’m focused on learning how to build website applications and
       serve it using Cloud Computing Technologies.
     </p>
     <a
       class="text-primary text-opacity-80 py-3 px-6
-      border border-primary rounded-sm inline-block"
+        border border-primary rounded-sm inline-block
+        md:text-lg md:px-8 md:py-4"
       href="#featured-projects"
     >
       View my projects
